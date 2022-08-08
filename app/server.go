ac7260adc6f3d7d3f2bd765e1ea984ca042cdcf8
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2022-08-08 11:16:50 +0200

    Respond with PONG

--- app/server.go
+++ app/server.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"os"
-
 	// Uncomment this block to pass the first stage
 	// "net"
 	// "os"
@@ -21,9 +21,22 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	connection, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	_, err = connection.Write(formatRESPString("PONG"))
+	if err != nil {
+		fmt.Println("Error writing to connection", err.Error())
+		os.Exit(1)
+	}
+}
+
+func formatRESPString(input string) []byte {
+	formattedString := "+" + input + "\r\n"
+	var buf bytes.Buffer
+	fmt.Fprintf(&buf, formattedString)
+	return buf.Bytes()
 }

271b68bcaa65dfebdd10050ab5683af550a86dfe
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2023-02-21 15:06:13 +0100

    Demo change

--- app/server.go
+++ app/server.go
@@ -13,6 +13,6 @@ func main() {
 	go instance.Listen("0.0.0.0:6379")
 	go instance.ProcessEventLoop()
 
-	fmt.Println("Server listening, press enter to stop")
+	fmt.Println("Server listening, press enter to stop this.")
 	fmt.Scanln()
 }
